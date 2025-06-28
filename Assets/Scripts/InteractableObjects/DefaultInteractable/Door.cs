9a2639113a1e976ea13fb34ba27feec4e24c8faf
loljek <brizingida851@gmail.com>
2025-06-29 02:06:06 +0300

    second test version
    some fixes 🔧

--- Assets/Scripts/InteractableObjects/DefaultInteractable/Door.cs
+++ Assets/Scripts/InteractableObjects/DefaultInteractable/Door.cs
@@ -1,48 +1,48 @@
-using System;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
-
-public class Door : InteractableObject
+namespace InteractableObjects.DefaultInteractable
 {
-    [SerializeField] private float openAngle = 90f;
-    [SerializeField] private float openSpeed = 2f;
-    [SerializeField] private BoxCollider doorColider;
-    public bool isOpen = false;
-    private bool isOpening = false;
-    private Quaternion openDirection;
+    [RequireComponent(typeof(BoxCollider))]
+
+    public class Door : InteractableObject
+    {
+        [SerializeField] private float openAngle = 90f;
+        [SerializeField] private float openSpeed = 2f;
+        [SerializeField] private BoxCollider doorCollider;
+        public bool isOpen;
+        private bool isOpening;
+        private Quaternion openDirection;
     
 
-    private void Awake()
-    {
-        interactSpace = doorColider;
-        type = "defaultObject";
-        openDirection = transform.rotation;
-    }
-
-    private void Update()
-    {
-        if (isOpening)
+        private void Awake()
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, openDirection, openSpeed * Time.deltaTime);
+            interactSpace = doorCollider;
+            openDirection = transform.rotation;
         }
-        if (Quaternion.Angle(transform.rotation, openDirection) < 0.1f)
+
+        private void Update()
         {
+            if (isOpening)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, openDirection, openSpeed * Time.deltaTime);
+            }
+
+            if (!(Quaternion.Angle(transform.rotation, openDirection) < 0.1f)) return;
             transform.rotation = openDirection;
             isOpening = false;
         }
-    }
-    public override void Interaction()
-    {
-        if (isOpen)
+        public override void Interaction()
         {
-            openDirection *= Quaternion.Euler(0, -openAngle, 0);
+            if (isOpen)
+            {
+                openDirection *= Quaternion.Euler(0, -openAngle, 0);
+            }
+            else
+            {
+                openDirection *= Quaternion.Euler(0, openAngle, 0);
+            }
+            isOpening = true;
+            isOpen = !isOpen;
         }
-        else
-        {
-            openDirection *= Quaternion.Euler(0, openAngle, 0);
-        }
-        isOpening = true;
-        isOpen = !isOpen;
     }
 }
